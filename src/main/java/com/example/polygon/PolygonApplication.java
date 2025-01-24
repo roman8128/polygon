2ce29a33198073cd98ab533e2484c4515dcaf4ca
roman8128 <stems_notice_0i@icloud.com>
2025-01-24 09:44:02 +0100

    initial commit

---
+++ src/main/java/com/example/polygon/PolygonApplication.java
@@ -0,0 +1,13 @@
+package com.example.polygon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolygonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolygonApplication.class, args);
+	}
+
+}
